14ef3c90b4d6b2dda7e4808d6cea879b9e004e13
Valentyna <valya0322@ukr.net>
2026-05-06 16:55:33 +0300

    refacted template literals with backticks

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,9 +1,3 @@
-export const Sum = ({ a = 0, b = 0 }) => {
-  const c = a + b;
-
-  return (
-    <p>
-      Sum of {a} and {b} is {c}
-    </p>
-  );
-};
+export const Sum = ({ a = 0, b = 0 }) => (
+  <p>{`Sum of ${a} and ${b} is ${a + b}`}</p>
+);
